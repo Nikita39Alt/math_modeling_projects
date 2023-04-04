61c36eeff191a46b1da1ff680153b03f27ba1a34
VladMaksimum <114826721+VladMaksimum@users.noreply.github.com>
2023-04-04 18:16:01 +0000

    project

--- project.py
+++ project.py
@@ -1,6 +1,41 @@
-import matplotlib.pyplot as plt
 import numpy as np
-#Земля
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+#Дифур
+frames = 60
+t=np.linspace(0, 300, frames)
+
+G = 6.67 * 10**(-11)
+M = 6 * 10**(24)
+m = 750
+k = 0.35
+
+def diff(w, t):
+    x, vx, y, vy = w
+    dxdt = vx
+    dvxdt = ((G * m * M * x) / (x**2 + y**2)**1.5) - (k * vx)
+    dydt = vy
+    dvydt = ((G * m * M * y) / (x**2 + y**2)**1.5) - (k * vy)
+    return dxdt, dvxdt, dydt, dvydt
+
+x0 = -240 *10**3
+vx0 = 21600 * np.cos(45)
+y0 = 240 *10**3
+vy0 = 21600 * np.sin(45)
+w0 = x0, vx0, y0, vy0
+
+sol = odeint(diff, w0, t)
+
+def move(i):
+    x= sol[i, 0]
+    y= sol[i, 2]
+    return x, y
+
+fig, ax = plt.subplots()
+
+#Земля с атмосферой
 angle = np.linspace( 0 , 2 * np.pi , 150 ) 
 
 radius_earth = 6378100
@@ -14,9 +49,16 @@ x_earth = radius_earth* np.cos(angle) + 0.5*radius_earth
 y_earth = radius_earth* np.sin(angle) - (np.sqrt(3)*radius_earth)/2
 plt.plot(x_earth, y_earth, color='g')
 
-edge  = 120000 * 2
+apparat, = plt.plot([], [], 'o', color = 'b')
+
+def animate(i):
+    apparat.set_data(move(i))
+
+ani = FuncAnimation(fig, animate, frames=frames, interval=30)
+
+edge  = 240 *10**3
 plt.axis('equal')
 plt.xlim(-edge, edge)
 plt.ylim(-edge, edge)
 
-plt.savefig('project')
+ani.save('project.gif')
