4edf78d169c3a147d1e3d64942f7767fe9da327f
VladMaksimum <114826721+VladMaksimum@users.noreply.github.com>
2023-04-10 21:08:58 +0000

    project

--- diff.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-from scipy.integrate import odeint
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
-
-def diff()
--- project.py
+++ project.py
@@ -8,8 +8,8 @@ from info import air_heatexchange
 from info import air_density
 
 #Дифур
-frames = 250
-t= np.linspace(0 , 100 , frames)
+frames = 100
+t = np.linspace(0 , 1000 , frames)
 
 G = 6.67 * 10**(-11)
 M = 6 * 10**(24)
@@ -26,30 +26,35 @@ c_ka = 897
 def diff(w, t):
     global radius_atmosphere
     x, vx, y, vy, T = w
-    if y < radius_atmosphere  and y > -radius_atmosphere and x <  radius_atmosphere and x > -radius_atmosphere:
-        dxdt = vx
-        dvxdt = (G * m * M * x) / ((x**2 + y**2)**1.5 * (x/(x**2 + y**2)) - vx * np.abs(vx) * air_density((x**2 + y**2) - radius_earth) * S * c_res / 2 / m)
-        dydt = vy
-        dvydt = -(G * m * M * y) / ((x**2 + y**2)**1.5 * (x/(x**2 + y**2)) - vy * np.abs(vy) * air_density((x**2 + y**2) - radius_earth) * S * c_res / 2 / m)
-        dTdt = T + np.sqrt(vx ** 2 + vy ** 2) * (air_temperature((x**2 + y**2) - radius_earth) - T) * air_heatexchange(air_temperature((x**2 + y**2) - radius_earth)) * air_density((x**2 + y**2) - radius_earth) * t / c_ka * (S_bok)
-        return dxdt, dvxdt, dydt, dvydt, dTdt
-    else:
-        dxdt = vx
-        dvxdt = ((G  * M * x) / (x**2 + y**2)**1.5)
-        dydt = vy
-        dvydt = -((G * M * y) / (x**2 + y**2)**1.5)
-        return dxdt, dvxdt, dydt, dvydt
-    #return dxdt, dvxdt, dydt, dvydt, dTdt
+    #if not(y < radius_atmosphere  and y > -radius_atmosphere and x <  radius_atmosphere and x > -radius_atmosphere):
+        #dxdt = vx
+        #dvxdt = ((G  * M * x) / (x**2 + y**2)**1.5)
+        #dydt = vy
+        #dvydt = -((G * M * y) / (x**2 + y**2)**1.5)
+    #else:
+    delta_x = x
+    delta_y = y 
+    r = np.sqrt(delta_x**2+delta_y**2)
+    sina = delta_x / r
+    cosa = delta_y / r
+
+    dxdt = vx
+    dvxdt = (G * M / (r ** 2) * cosa - vx * np.abs(vx) * air_density(r - radius_earth) * S * c_res / 2 / m) * x
+    dydt = vy
+    dvydt = -(G * M / (r ** 2) * cosa - vy * np.abs(vy) * air_density(r - radius_earth) * S * c_res / 2 / m) * y
+    dTdt = T + np.sqrt(vx ** 2 + vy ** 2) * (air_temperature((x**2 + y**2) - radius_earth) - T) * air_heatexchange(air_temperature((x**2 + y**2) - radius_earth)) * air_density((x**2 + y**2) - radius_earth) * t / c_ka * (S_bok)
+    return dxdt, dvxdt, dydt, dvydt, dTdt
 
 
-x0 = -radius_atmosphere 
-vx0 = 8000 * np.cos(60)
-y0 = radius_atmosphere 
-vy0 = -8000 * np.sin(60)
+x0 = radius_atmosphere * np.cos(60) 
+vx0 = 8000 * np.cos(300)
+y0 = radius_atmosphere * np.sin(60)
+vy0 = -8000 * np.sin(300)
 T0 = 2.7
 w0 = x0, vx0, y0, vy0, T0
 
 sol = odeint(diff, w0, t)
+print(sol)
 
 def move(i):
     x= sol[i, 0]
@@ -80,7 +85,7 @@ def animate(i):
 
 ani = FuncAnimation(fig, animate, frames=frames, interval=30)
 
-edge  =  radius_atmosphere * 3
+edge  =  radius_atmosphere * 2
 plt.axis('equal')
 plt.xlim(-edge, edge)
 plt.ylim(-edge, edge)
